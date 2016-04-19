e174e19ec443c4ed76711dc75ea98639bccaafc4
Eric Avdey <eiri@eiri.ca>
2016-04-18 22:14:56 -0300

    Eunit on an empty list

--- test/list_as_table_tests.erl
+++ test/list_as_table_tests.erl
@@ -2,6 +2,28 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-main_test() ->
-  ?debugVal( list_as_table:print([[{a, 1}]]) ),
-  ?assert(true).
+empty_test_() ->
+  {setup,
+    fun() ->
+      TmpFile = lib:nonl(os:cmd("mktemp")),
+      {ok, IoDevice} = file:open(TmpFile, [read, write]),
+      {ok, TmpFile, IoDevice}
+    end,
+    fun({ok, TmpFile, IoDevice}) ->
+      ok = file:close(IoDevice),
+      ok = file:delete(TmpFile)
+    end,
+    fun({ok, TmpFile, IoDevice}) ->
+      [
+        ?_test(begin
+          list_as_table:print(IoDevice, []),
+          Expect = <<"+-------------+\n"
+            "| empty list  |\n"
+            "+-------------+\n"
+            "+-------------+\n">>,
+          {ok, Table} = file:read_file(TmpFile),
+          ?assertEqual(Expect, Table)
+        end)
+      ]
+    end
+  }.
